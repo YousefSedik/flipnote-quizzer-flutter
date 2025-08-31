bfeb2b38eb6253d0132540a6ce47c2f7ba2480ff
YousefSedik <maxxd1919@gmail.com>
2025-08-31 13:57:32 +0300

    feat: added pickAndReadPdf

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,2 +1,10 @@
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'dart:io';
 
-
+Future<String> pickAndReadPdf(String result) async {
+  File file = File(result);
+  final PdfDocument document = PdfDocument(
+    inputBytes: await file.readAsBytes(),
+  );
+  return PdfTextExtractor(document).extractText();
+}
